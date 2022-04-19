b8d809b7a2dc8d71a1995e521185429664d2b1be
Ashish Kumar Jha <aj310161@gmail.com>
2022-04-20 01:38:06 +0530

    Git Action

---
+++ src/main/java/com/Deploy/GitActionCiCd/GitActionCiCdApplication.java
@@ -0,0 +1,21 @@
+package com.Deploy.GitActionCiCd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class GitActionCiCdApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "GIT-CI/CD PIPELINE USING GIT ACTIONS";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionCiCdApplication.class, args);
+	}
+
+}
