492d78f539bbe2f3bf4dec52b0dd186ae81b3e6d
Ashish Kumar Jha <aj310161@gmail.com>
2022-04-24 00:12:44 +0530

    api chnages updated

--- src/main/java/com/Deploy/GitActionCiCd/GitActionCiCdApplication.java
+++ src/main/java/com/Deploy/GitActionCiCd/GitActionCiCdApplication.java
@@ -1,5 +1,6 @@
 package com.Deploy.GitActionCiCd;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GitActionCiCdApplication {
 
-	@GetMapping("/")
+	@Value("${spring.datasource.username}")
+	String projName;
+
+	@Value("${server.port}")
+	String portNo;
+	@GetMapping("/pipeline-info")
 	public String welcome(){
-		return "GIT-CI/CD PIPELINE USING GIT ACTIONS";
+		return "GIT-CI/CD PIPELINE USING GIT ACTIONS TRIGGRED \r\n Name: "+projName + " \r\n Port: "+ portNo;
+
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionCiCdApplication.class, args);
