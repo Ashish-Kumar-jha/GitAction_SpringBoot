ea124ab58d1daa9292dd6c30e72409f66d53d7a9
Ashish Kumar Jha <aj310161@gmail.com>
2022-05-28 01:59:29 +0530

    config test

--- src/main/java/com/Deploy/GitActionCiCd/Entity/Student.java
+++ src/main/java/com/Deploy/GitActionCiCd/Entity/Student.java
@@ -4,17 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name ="STUD")
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
